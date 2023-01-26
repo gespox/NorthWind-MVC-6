8e67bd1a0f9f9962c3f636501358f571bb052615
Sait Okan <okanmehmetsait@gmail.com>
2023-01-26 11:26:15 +0300

    Add project files.

---
+++ Northwind-mvc/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Northwind_mvc.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<NorthwindContext>(options =>
+                     options.UseSqlServer(builder.Configuration.GetConnectionString("NorthwindContext")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
